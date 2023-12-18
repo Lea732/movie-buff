ae4ddede461ec2fd085ed74274b778e45f393874
Lea732 <leapoulin07@gmail.com>
2023-12-18 19:39:17 +0100

    Added useEffect alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import Header from './components/Header'
+import { useEffect } from 'react';
 
 import './App.css'
 import HomePresentation from './components/HomePresentation'
 
 
 function App() {
+  useEffect(() => {
+    alert("Hello movies and series lover ! :)");
+  }, []);
 
   return (
-
     <>  
       <Header/>
-    </>
+   
 
     <HomePresentation/>
-
+    </>
   )
 }
 
