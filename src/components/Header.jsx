7a39d6246dde756cca2a6703d8d8e3dab549e21f
marcopoltergeist <paul_dubor@outlook.fr>
2023-12-18 16:45:44 +0100

    init header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
+import Logo from "./Logo";
+import "../styles/Header.css"
+import SelectorFilmSerie from "./SelectorFilmSerie";
+
 function Header() {
     return <header>
-        <h1>Movie-buff</h1>
+        <Logo/>
+        <h1>Movie-buff</h1> 
+        <SelectorFilmSerie/>
     </header>
 }
 
---
+++ src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import Camera from '../assets/camera.png'
+
+function Logo() {
+    return <img src={Camera} alt="Camera" />
+}
+
+export default Logo;
