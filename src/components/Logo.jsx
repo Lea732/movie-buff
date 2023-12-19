9e5472c5db3ddda74547e63fc2c8316daa74e646
Lea732 <leapoulin07@gmail.com>
2023-12-19 10:59:19 +0100

    Added style

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import Camera from '../assets/camera.png'
 
 function Logo() {
-    return <img src={Camera} alt="Camera" />
+    return <img src={Camera} alt="Camera" className='logo'/>
 }
 
 export default Logo;
